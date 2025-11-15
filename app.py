005accd25f8ae18adba06f83cf6dfcacf3b68d6b
aalvaropc <aalvaropc@gmail.com>
2025-11-15 04:13:19 -0500

    test pipeline

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def get_current_time():
     current_time = time.strftime("%Y-%m-%d %H:%M:$S", time.localtime())
-    return f"La hora actual es: {current_time}"
+    return f"La hora actual es: {current_time}!"
 
 
 if __name__ == "__main__":
